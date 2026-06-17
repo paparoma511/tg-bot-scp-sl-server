ae59c08c824a358e79236e14814d3a4cc842c50c
paparoma511 <paparoma511@gmail.com>
2026-06-17 11:05:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-ADMINS = [6290782095]
-COOLDOWN = 60 * 60 * 24
+ADMINS = [6290782095]  # сюда свой Telegram ID
+
+COOLDOWN = 60 * 60 * 24  # 24 часа
