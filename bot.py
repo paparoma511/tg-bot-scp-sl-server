57be05e7c9dfffea885e08ce42ead2c3209ce9ee
paparoma511 <paparoma511@gmail.com>
2026-06-17 13:45:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,25 @@
 import asyncio
 import logging
-import os
 
 from aiogram import Bot, Dispatcher
-from dotenv import load_dotenv
-
-load_dotenv()
-
 from config import BOT_TOKEN
+
+from handlers.start import router as start_router
+from handlers.open import router as open_router
+from handlers.cards import router as cards_router
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-# подключаем хендлеры
-import handlers.start
-import handlers.open
-import handlers.cards
+dp.include_router(start_router)
+dp.include_router(open_router)
+dp.include_router(cards_router)
 
 logging.basicConfig(level=logging.INFO)
 
 async def main():
     print("BOT STARTED")
 
-    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
