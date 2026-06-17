fa9cf13ad82c39d394b2a6dab4649930f4d5f401
paparoma511 <paparoma511@gmail.com>
2026-06-17 11:05:09 +0300

    Create data/cards.py

---
+++ data/cards.py
@@ -0,0 +1,15 @@
+CARDS = {
+    "🟢 Common": ["Class-D", "Scientist", "Guard", "Medkit"],
+    "🔵 Rare": ["MTF Private", "Chaos Rifleman", "Keycard L2"],
+    "🟣 Epic": ["SCP-173", "SCP-049", "E-11 Rifle"],
+    "🟡 Legendary": ["SCP-096", "SCP-106", "Micro H.I.D."],
+    "🔴 Mythic": ["SCP-682", "SCP-999", "SCP-001"]
+}
+
+WEIGHTS = {
+    "🟢 Common": 60,
+    "🔵 Rare": 25,
+    "🟣 Epic": 10,
+    "🟡 Legendary": 4,
+    "🔴 Mythic": 1
+}
