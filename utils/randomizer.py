fd5db340702f5b6349ed8afbee94dca5cbe41ae6
paparoma511 <paparoma511@gmail.com>
2026-06-17 13:51:31 +0300

    Delete randomizer.py

--- utils/randomizer.py
+++
@@ -1,8 +0,0 @@
-import random
-from data.cards import WEIGHTS
-
-def roll_rarity():
-    pool = []
-    for rarity, weight in WEIGHTS.items():
-        pool.extend([rarity] * weight)
-    return random.choice(pool)
