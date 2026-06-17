1903be4662ed904c23f8ee01962dabb20156d0cc
paparoma511 <paparoma511@gmail.com>
2026-06-17 23:38:04 +0300

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,85 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+from database import db
+
+router = Router()
+
+
+@router.message(Command("profile"))
+async def profile(message: Message):
+
+    conn = db()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT cards_opened
+        FROM users
+        WHERE user_id=?
+        """,
+        (
+            message.from_user.id,
+        )
+    )
+
+    row = cur.fetchone()
+
+    cur.execute(
+        """
+        SELECT COUNT(*)
+        FROM cards
+        WHERE user_id=?
+        """,
+        (
+            message.from_user.id,
+        )
+    )
+
+    cards_count = cur.fetchone()[0]
+
+    conn.close()
+
+    await message.answer(
+        f"👤 {message.from_user.full_name}\n\n"
+        f"🃏 Карт: {cards_count}\n"
+        f"🎁 Открыто карт: {row[0]}"
+    )
+
+
+@router.message(Command("top"))
+async def top(message: Message):
+
+    conn = db()
+    cur = conn.cursor()
+
+    cur.execute("""
+    SELECT
+        username,
+        user_id,
+        COUNT(cards.id)
+    FROM users
+    LEFT JOIN cards
+        ON users.user_id = cards.user_id
+    GROUP BY users.user_id
+    ORDER BY COUNT(cards.id) DESC
+    LIMIT 10
+    """)
+
+    data = cur.fetchall()
+
+    conn.close()
+
+    text = "🏆 Топ игроков\n\n"
+
+    for place, (username, user_id, count) in enumerate(data, start=1):
+
+        if username:
+            name = f"@{username}"
+        else:
+            name = f"ID {user_id}"
+
+        text += f"{place}. {name} — {count} карт\n"
+
+    await message.answer(text)
